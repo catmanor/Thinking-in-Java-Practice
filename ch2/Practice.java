709e038027f82394a27fc30de0a83b8a9c2fe18a
keeponcoding <52342021+garbagecenter@users.noreply.github.com>
2021-01-11 18:32:35 +0800

    chapter 2

---
+++ ch2/Practice.java
@@ -0,0 +1,10 @@
+public class Practice {
+    public static void main(String[] args)
+    {
+        int anInt = 5;
+        char aChar = 's';
+
+        System.out.println(anInt);
+        System.out.println(aChar);
+    }
+}
